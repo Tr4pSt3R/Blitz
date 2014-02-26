f7335f374f55e45104ca3267724979b6128d9fbb
Tr4pSt3R <mightyj@hotmail.co.uk>
2014-02-26 11:51:23 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blitz::Application.config.secret_key_base = 'e3c98430bd70d1a2058278ae3e19f4f9854821a7ce95ec42c49adae2085e82f7adb6f3c8ba998f2f683a32235eb7568373d46ce17ef16e021098a6b8a8961cf2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blitz::Application.config.session_store :cookie_store, key: '_Blitz_session'
